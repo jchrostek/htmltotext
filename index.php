e8a7f0777aef81d7f08389387ba28a54b628ae2f
Jakub Chrostek <jakubchrostek@wp.pl>
2017-09-22 09:47:35 +0200

    Strona kodowania
    Ustawiono stronę kodowania

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<meta charset="utf-8">
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
